76206d226614dba20f896e1d4d4bebd76e17f2b2
Tom Ray <tomwray13@gmail.com>
2022-11-04 09:38:36 +0000

    Remove unrequired files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TweetsModule } from './modules/tweets/tweets.module';
 
 @Module({
   imports: [TweetsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
